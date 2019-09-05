8fd90ebdb8cf2af78b660e47b8b22f455873be79
unknown <owdelc@gmail.com>
2019-09-04 21:14:09 -0600

    ya solo falta el main

---
+++ clases/tips.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class tips{
+  System.out.println("Consejos de Plantación de Arboles");
+  System.out.println("");
+  System.out.println("Guatemala es un pais con muchas subregiones tropicales las cuales");
+  System.out.println("son templada, calido y frío. Por lo tanto te daremos recomendaciones");
+  System.out.println("de arboles aptos para cada región y una guia general para plantarlos.");
+
+  
+}
