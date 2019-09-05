05ec7a62a0d798fe57be15695b5508fb9067a137
unknown <owdelc@gmail.com>
2019-09-04 19:54:51 -0600

    carpeta de clases

---
+++ clases/contacto.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+public class contacto{
+  System.out.println("Contacto de Voluntariado");
+  System.out.println("");
+  System.out.println("Amigos del Lago de Atitlan (amigosatitlan.org)");
+  System.out.println("Tel: 2362-3195    E-mail: info@amigosatitlan.org ");
+  System.out.println("");
+  System.out.println("Defensores De La Naturaleza (defensores.org.gt)");
+  System.out.println("Tel: 2310-2929    E-mail: info@defensores.orgt.gt");
+  System.out.println("");
+  System.out.println("Guateambiente  (guateambiente.org)");
+  System.out.println("Tel: 4095-9871    E-mail: luisrodriguez@guateambiente.org");
+  System.out.println("");
+  System.out.println("Fundaeco  (fundaeco.org.gt)");
+  System.out.println("Tel: 2314-1900    E-mail: m.cerezo@fundaeco.org.gt");
+
+}
